6d3fb8c90026e385a42cd04b140fa365495e3d22
lr080 <lr080@hdm-stuttgart.de>
2025-11-15 00:18:31 +0100

    improved controller

--- src/ship.gd
+++ src/ship.gd
@@ -23,6 +23,9 @@ func _process(_delta: float) -> void:
 	winkel += lenkinput * _delta * (speed / 10.0);
 	speed += speedinput * _delta * 40.0;
 	
+
+	#$"../RigidBody3D".add_constant_force(transform.basis.x * lenkinput * _delta * 20.0, transform.basis.y * 2.0);
+	
 	global_transform.origin += -global_transform.basis.z * speed * _delta;
 	
 	var global_2d := Vector2(self.global_position.x, self.global_position.z)
@@ -31,7 +34,7 @@ func _process(_delta: float) -> void:
 	var normal = calculateNormal(global_2d)
 	
 	var basis = Basis()               # identity basis
-	basis.y = normal.normalized()     # set the UP direction
+	basis.y = normal.normalized().rotated(global_basis.z, lenkinput / 4.0).rotated(global_basis.x, -0.15 * abs(lenkinput))     # set the UP direction
 	basis.x = basis.y.cross(-Vector3.FORWARD.rotated(Vector3(0, 1, 0), winkel)).normalized()
 	basis.z = basis.x.cross(basis.y).normalized()
 
