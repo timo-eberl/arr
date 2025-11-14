2ec5aa9097ddc7d855d670bbc2a682b23ec3a99f
lr080 <lr080@hdm-stuttgart.de>
2025-11-14 20:00:11 +0100

    Hinge joint

--- src/ship.gd
+++ src/ship.gd
@@ -4,3 +4,9 @@ func height(xz: Vector2) -> float:
 	var time_ms := Time.get_ticks_msec()
 	return ((sin(xz.y * 0.3 + time_ms * 0.001)) * 1.0) \
 		+ ((sin(-xz.x * 0.2 + time_ms * 0.001)) * 0.5);
+
+func _process(delta):
+	
+	
+	pass
+	
