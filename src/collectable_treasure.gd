76537735043c81be178024f22706c92beabe9253
Timo Eberl <timo@timoeberl.de>
2025-11-16 08:08:58 +0100

    more tweaks

--- src/collectable_treasure.gd
+++ src/collectable_treasure.gd
@@ -7,5 +7,7 @@ func _physics_process(delta: float) -> void:
 	var target_y := WaveHeight.height(
 		Vector2(self.global_position.x, self.global_position.z)
 	)
-	if self.global_position.y < target_y:
+	if self.global_position.y < target_y and (target_y - self.global_position.y) > 1.0:
 		self.global_position.y += delta * 3.0
+	else:
+		self.global_position.y = lerp(global_position.y, target_y, delta * 3)
