985161cf4d8ffc78a61d8fce993bf8a4ce72c0e0
Johannes Schultz <sagamusix@users.noreply.github.com>
2025-11-16 08:58:06 +0100

    we're sinking, we're sinking!

--- treasure/collected_treasure.gd
+++ treasure/collected_treasure.gd
@@ -1,5 +1,19 @@
 extends RigidBody3D
 
+var splash_sounds = ["res://Sounds/splash1.wav", "res://Sounds/splash2.wav", "res://Sounds/splash3.wav", "res://Sounds/splash4.wav"]
+var splash_sound_player: AudioStreamPlayer3D = AudioStreamPlayer3D.new()
+
+func _ready() -> void:
+	splash_sound_player.stream = load(splash_sounds.pick_random())
+	get_tree().root.add_child(splash_sound_player)
+
 func _process(_delta: float) -> void:
 	if self.global_position.y < -10.0:
 		queue_free()
+		
+		splash_sound_player.position = self.global_position
+		splash_sound_player.volume_db = -12
+		splash_sound_player.play()
+		
+		await splash_sound_player.finished
+		splash_sound_player.queue_free()
